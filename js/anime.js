c61c839909cfa21970bf4cefbf534ae6c36099a0
Hossam-salah88 <eng.hegazy88@gmail.com>
2021-11-27 01:27:36 +0200

    add prettier fill

--- js/anime.js
+++ js/anime.js
@@ -1,50 +1,54 @@
-document.addEventListener('DOMContentLoaded', () => {
-	//Intitial page load animation
-	anime.timeline({
-		easing: 'easeOutExpo',
-	})
-	.add({
-		targets: 'header',
-		width: ['0px', '100%'],
-	})
-
+document.addEventListener("DOMContentLoaded", () => {
+  //Intitial page load animation
+  anime
+    .timeline({
+      easing: "easeOutExpo",
+    })
     .add({
-        targets: '.nav__item, .nav__btn div',
-        translateY: [20, 0],
-        opacity: [0, 1],
-        delay: (el, i) => 100 * i,
-		offset: '-=700',
+      targets: "header",
+      width: ["0px", "100%"],
     })
 
     .add({
-        targets: '.home__title, .home__subtitle, .home__info, .home__data div',
-        translateY: [10, 0],
-        easing: 'spring(1, 80, 8, 30)',
-        delay: (el, i) => 100 * i,
-        offset: '-=700',
-        opacity: [0, 1],
+      targets: ".nav__item, .nav__btn div",
+      translateY: [20, 0],
+      opacity: [0, 1],
+      delay: (el, i) => 100 * i,
+      offset: "-=700",
     })
 
     .add({
-        targets: '.home__img',
+      targets: ".home__title, .home__subtitle, .home__info, .home__data div",
+      translateY: [10, 0],
+      easing: "spring(1, 80, 8, 30)",
+      delay: (el, i) => 100 * i,
+      offset: "-=700",
+      opacity: [0, 1],
+    })
+
+    .add(
+      {
+        targets: ".home__img",
         translateX: [20, 0],
-        easing: 'spring(1, 80, 8, 30)',
+        easing: "spring(1, 80, 8, 30)",
         opacity: [0, 1],
-    }, 2500)
+      },
+      2500
+    )
 
     .add({
-        targets: '.chooseUs__title, .chooseUs__subtitle, .chooseUs__paragraph, .chooseUs__data div',
-        translateY: [10, 0],
-        easing: 'spring(1, 80, 8, 30)',
-        delay: (el, i) => 100 * i,
-        opacity: [0, 1],
+      targets:
+        ".chooseUs__title, .chooseUs__subtitle, .chooseUs__paragraph, .chooseUs__data div",
+      translateY: [10, 0],
+      easing: "spring(1, 80, 8, 30)",
+      delay: (el, i) => 100 * i,
+      opacity: [0, 1],
     })
 
     .add({
-        targets: '.chooseUs__img',
-        translateX: [20, 0],
-        easing: 'spring(1, 80, 8, 30)',
-        opacity: [0, 1],
-    })
-})
-
+      targets: ".chooseUs__img",
+      translateX: [20, 0],
+      easing: "spring(1, 80, 8, 30)",
+      opacity: [0, 1],
+    });
+});
--- js/script.js
+++ js/script.js
@@ -2,76 +2,75 @@
 const searchForm = document.querySelector(".search__form");
 const searchBtn = document.querySelector("#search-btn");
 
-searchBtn.addEventListener('click', () => {
-    searchForm.classList.toggle('active')
-    shoppinCard.classList.remove('active')
-    loginForm.classList.remove('active')
-    navList.classList.remove('active')
-})
+searchBtn.addEventListener("click", () => {
+  searchForm.classList.toggle("active");
+  shoppinCard.classList.remove("active");
+  loginForm.classList.remove("active");
+  navList.classList.remove("active");
+});
 
 // ================ sHow Shopping CArd ==================
 
-const shoppinCard = document.querySelector('.shoppingCard');
-const shoppinCardBtn = document.querySelector('#cart-btn');
+const shoppinCard = document.querySelector(".shoppingCard");
+const shoppinCardBtn = document.querySelector("#cart-btn");
 
-shoppinCardBtn.addEventListener('click', () => {
-    shoppinCard.classList.toggle('active')
-    searchForm.classList.remove('active')
-    loginForm.classList.remove('active')
-    navList.classList.remove('active')
-})
+shoppinCardBtn.addEventListener("click", () => {
+  shoppinCard.classList.toggle("active");
+  searchForm.classList.remove("active");
+  loginForm.classList.remove("active");
+  navList.classList.remove("active");
+});
 
 // =================== Show login form ==============
 
-const loginForm = document.querySelector('.login');
-const loginBtn = document.querySelector('#login-btn')
+const loginForm = document.querySelector(".login");
+const loginBtn = document.querySelector("#login-btn");
 
-loginBtn.addEventListener('click', () => {
-    loginForm.classList.toggle('active')
-    searchForm.classList.remove('active')
-    shoppinCard.classList.remove('active')
-    navList.classList.remove('active')
-})
+loginBtn.addEventListener("click", () => {
+  loginForm.classList.toggle("active");
+  searchForm.classList.remove("active");
+  shoppinCard.classList.remove("active");
+  navList.classList.remove("active");
+});
 
 // ================= Show men list =================
 
-const navList = document.querySelector('.nav__list');
-const menuBtn = document.querySelector('#menu-btn');
+const navList = document.querySelector(".nav__list");
+const menuBtn = document.querySelector("#menu-btn");
 
-menuBtn.addEventListener('click', () => {
-    navList.classList.toggle('active')
-    searchForm.classList.remove('active')
-    shoppinCard.classList.remove('active')
-    loginForm.classList.remove('active')
-})
+menuBtn.addEventListener("click", () => {
+  navList.classList.toggle("active");
+  searchForm.classList.remove("active");
+  shoppinCard.classList.remove("active");
+  loginForm.classList.remove("active");
+});
 
 // ============= remove menu at on scroll ===============
 
-const scrollUp = document.querySelector('#scroll-top');
+const scrollUp = document.querySelector("#scroll-top");
 
 window.onscroll = () => {
-    navList.classList.remove('active');
+  navList.classList.remove("active");
 
-    // ================== Scroll top button ===============================
+  // ================== Scroll top button ===============================
 
-
-    if(window.scrollY > 40) {
-        scrollUp.classList.add('active')
-    }else {
-        scrollUp.classList.remove('active')
-    }
-}
+  if (window.scrollY > 40) {
+    scrollUp.classList.add("active");
+  } else {
+    scrollUp.classList.remove("active");
+  }
+};
 
 // ================= move the img ============================
-const home = document.querySelector('.home');
-const parllImg = document.querySelector('.home__img-parallImg');
+const home = document.querySelector(".home");
+const parllImg = document.querySelector(".home__img-parallImg");
 
 home.onmousemove = (e) => {
-    let x = (window.innerWidth - e.pageX * 2) / 90;
-    let y = (window.innerHeight - e.pageY * 2) / 90;
+  let x = (window.innerWidth - e.pageX * 2) / 90;
+  let y = (window.innerHeight - e.pageY * 2) / 90;
 
-    parllImg.style.transform = `translateX(${y}px) translateY(${x}px)`
-}
+  parllImg.style.transform = `translateX(${y}px) translateY(${x}px)`;
+};
 
 // // ============================ loder Section =====================
 // const loaderContainer = document.querySelector('.loader')
